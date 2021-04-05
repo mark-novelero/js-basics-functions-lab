a6a643ff03e029a9aaaf8d7546a8e710b9907e3b
mark novelero <mark.novelero@gmail.com>
2021-04-05 17:47:12 -0400

    commit tree 160e0ffbf8b73a12aa51e8f564d7f98d6af22d43

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Code your solution in this file!
+
+
+function distanceFromHqInBlocks(pickUp= 34){
+    let hq = 42;
+    return Math.abs(pickUp - hq); 
+}
+
+function distanceFromHqInFeet(pickUp = 50, hq = 42){
+    let disBlock = pickUp - hq; 
+    let inFeet = disBlock * 264; 
+    return Math.abs(inFeet); 
+}
+
+function distanceTravelledInFeet(start = 50, end = 60){
+    let disBlock = end - start; 
+    let inFeet = disBlock * 264; 
+    return Math.abs(inFeet);
+}
+
+function calculatesFarePrice(start = 34, end = 24){
+    let tBlocks = end - start; 
+    let feet = Math.abs(tBlocks) * 264; 
+    if(feet <= 400){
+        return 0; 
+    } else if (feet > 400 && feet < 2000){
+        let feetAfterFree = feet - 400; 
+        return feetAfterFree * .02; 
+    } else if(feet > 2000 && feet < 2500){
+        return 25; 
+    } else{
+        return 'cannot travel that far'
+    }
+}
